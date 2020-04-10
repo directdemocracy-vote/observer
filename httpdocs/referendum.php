e70e61c88385485dab29647f9b07cf48c9a57c3e
Galine <webmaster@cyberbotics.com>
2020-04-10 23:09:59 +0200

    referendum

---
+++ httpdocs/referendum.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="https://directdemocracy.vote/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://directdemocracy.vote/css/directdemocracy.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://directdemocracy.vote/js/crypto-js.js"></script>
+    <title>observer.directdemocracy.vote</title>
+  </head>
+  <body>
+    <div class='corner-ribbon' title="This web site is in beta quality: it may have bugs and change without notice. Please, report any problem to info@<?=$base_domain?>.">Beta</div>
+    <main role='main'>
+      <div class="jumbotron directdemocracy-title">
+        <div class="container">
+          <h1>Referendum results
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="container">
+          <h2>Title</h2>
+        </div>
+      </div>
+    </main>
+    <div>
+      <hr>
+      <footer>
+        <p style='text-align:center'><small>Made by citizens for citizens</small></p>
+      </footer>
+    </div>
+  </body>
+</html>
